a6347b7831345ccde07cf858bd06c153b5b47902
yinchong <yin.chong@intellif.com>
2018-12-21 12:32:34 +0800

    添加详细日志

--- src/main/java/cn/mst/server/net/MstServerHandler.java
+++ src/main/java/cn/mst/server/net/MstServerHandler.java
@@ -43,7 +43,7 @@ public class MstServerHandler extends SimpleChannelInboundHandler<String> {
             Integer state = entry.getKey();
             String token = entry.getValue();
             if(state!=MstMessageBuilder.PING){
-                logger.info(SystemConstant.PREV_LOG+msg);
+                logger.info(SystemConstant.SERVER_LOG+msg);
             }
             switch (state){
                 case MstMessageBuilder.REGISTER:
