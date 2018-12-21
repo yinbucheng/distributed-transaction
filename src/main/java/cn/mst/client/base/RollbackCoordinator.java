20410cadc150e47133dd975bde28930d48d2d19e
yinchong <yin.chong@intellif.com>
2018-12-21 14:47:30 +0800

    优化代码日志

--- src/main/java/cn/mst/client/base/RollbackCoordinator.java
+++ src/main/java/cn/mst/client/base/RollbackCoordinator.java
@@ -5,9 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.SQLException;
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Map;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
