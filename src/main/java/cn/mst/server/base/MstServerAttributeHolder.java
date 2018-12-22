d0e345c1281251e6f79654856ea82d9a27edcc68
yinchong <yin.chong@intellif.com>
2018-12-22 15:27:10 +0800

    修改master上面readme

--- src/main/java/cn/mst/server/base/MstServerAttributeHolder.java
+++ src/main/java/cn/mst/server/base/MstServerAttributeHolder.java
@@ -1,8 +1,12 @@
 package cn.mst.server.base;
 
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.Signal;
+import io.netty.util.concurrent.DefaultPromise;
 import org.apache.zookeeper.ZooKeeper;
 
+import java.lang.reflect.Field;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
@@ -17,6 +21,27 @@ public class MstServerAttributeHolder {
     private static LinkedBlockingQueue<String> rollBack = new LinkedBlockingQueue<>();
     private static ConcurrentHashMap<String,List<ChannelHandlerContext>> token_channels = new ConcurrentHashMap<>();
     private static volatile ZooKeeper zkClient;
+
+    private static volatile ChannelFuture closeFuture;
+
+    public static void addCloseFuture(ChannelFuture future){
+        closeFuture = future;
+    }
+
+    public static void notifyCloseFutrue(){
+        if(closeFuture==null)
+            return;
+        synchronized (closeFuture){
+            try {
+                Field field = DefaultPromise.class.getDeclaredField("result");
+                field.setAccessible(true);
+                field.set(closeFuture, Signal.valueOf(DefaultPromise.class, "SUCCESS"));
+                closeFuture.notifyAll();
+            }catch (Exception e){
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
     public static void addRollBackFlag(String token){
         rollBack.add(token);
